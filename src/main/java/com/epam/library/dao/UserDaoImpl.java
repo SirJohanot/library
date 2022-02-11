05b48687305d0ac84a4bb35937e73e960cc66374
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-02-11 10:13:01 +0300

    currently, just trying to test the login method in UserServiceImpl

--- src/main/java/com/epam/library/dao/UserDaoImpl.java
+++ src/main/java/com/epam/library/dao/UserDaoImpl.java
@@ -2,7 +2,6 @@ package com.epam.library.dao;
 
 import com.epam.library.entity.User;
 import com.epam.library.exception.DaoException;
-import com.epam.library.mapper.RowMapperFactory;
 import com.epam.library.mapper.UserRowMapper;
 
 import java.sql.Connection;
@@ -11,9 +10,7 @@ import java.util.Optional;
 
 public class UserDaoImpl extends AbstractDao<User> implements UserDao {
 
-    private static final String FIND_BY_LOGIN_AND_PASSWORD = "SELECT * FROM user WHERE login = ? AND password = MD5('?')";
-
-    private final RowMapperFactory rowMapperFactory = new RowMapperFactory();
+    private static final String FIND_BY_LOGIN_AND_PASSWORD = "select * from user where login = ? and password = md5(?);";
 
     public UserDaoImpl(Connection connection) {
         super(connection);
