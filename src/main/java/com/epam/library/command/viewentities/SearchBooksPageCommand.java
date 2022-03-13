a6c3fdc203a46126694af802435ecc62e276f345
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-03-13 16:52:32 +0300

    added SearchBooksPageCommand

---
+++ src/main/java/com/epam/library/command/viewentities/SearchBooksPageCommand.java
@@ -0,0 +1,35 @@
+package com.epam.library.command.viewentities;
+
+import com.epam.library.constant.AttributeNameConstants;
+import com.epam.library.constant.PagePathConstants;
+import com.epam.library.constant.ParameterNameConstants;
+import com.epam.library.entity.book.Book;
+import com.epam.library.exception.ServiceException;
+import com.epam.library.pagination.Paginator;
+import com.epam.library.service.BookService;
+import com.epam.library.specification.BookContainsLineSpecification;
+import com.epam.library.specification.Specification;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public class SearchBooksPageCommand extends AbstractViewPageCommand<Book> {
+
+    private static final int BOOKS_PER_PAGE = 5;
+
+    private final BookService bookService;
+
+    public SearchBooksPageCommand(BookService bookService, Paginator<Book> paginator) {
+        super(paginator, BOOKS_PER_PAGE, AttributeNameConstants.BOOK_LIST, PagePathConstants.SEARCH_BOOKS);
+        this.bookService = bookService;
+    }
+
+    @Override
+    protected List<Book> getEntitiesUsingService(HttpServletRequest req) throws ServiceException {
+        String searchValue = req.getParameter(ParameterNameConstants.SEARCH_VALUE);
+        req.setAttribute(AttributeNameConstants.SEARCH_VALUE, searchValue);
+        Specification<Book> bookSearchSpecification = new BookContainsLineSpecification(searchValue);
+
+        return bookService.getAllSpecifiedBooks(bookSearchSpecification);
+    }
+}
