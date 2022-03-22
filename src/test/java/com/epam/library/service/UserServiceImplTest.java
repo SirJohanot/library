5dbe04e14afe33154df97e176d7903cfc82ad840
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-03-22 19:16:52 +0300

    reworked tests in UserServiceImplTest

--- src/test/java/com/epam/library/service/UserServiceImplTest.java
+++ src/test/java/com/epam/library/service/UserServiceImplTest.java
@@ -8,13 +8,15 @@ import com.epam.library.entity.enumeration.UserRole;
 import com.epam.library.exception.DaoException;
 import com.epam.library.exception.ServiceException;
 import com.epam.library.exception.ValidationException;
-import com.epam.library.specification.NoSpecification;
+import com.epam.library.specification.UserContainsLineSpecification;
+import com.epam.library.validation.PasswordValidator;
 import com.epam.library.validation.UserValidator;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -67,13 +69,102 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void testGetAllSpecifiedUsersShouldDelegateToUserDaoCreatedByHelperWhenThereIsNoSpecification() throws DaoException, ServiceException {
+    public void testSignUpShouldInvokeSaveWithPasswordMethodOfUserDaoWhenTheUserAndThePasswordAreValidAndAUserWithTheSameLoginDoesNotExist() throws DaoException, ValidationException, ServiceException {
         //given
-        User expectedUser = new User(userId, login, name, surname, role, blocked);
-        List<User> expectedUserList = List.of(expectedUser);
-        when(userDao.getAll()).thenReturn(expectedUserList);
+        String password = "Password";
+
+        PasswordValidator passwordValidator = mock(PasswordValidator.class);
+        doNothing().when(passwordValidator).validate(password);
+
+        User expectedUserToGetSaved = new User(null, login, name, surname, UserRole.READER, false);
+        UserValidator userValidator = mock(UserValidator.class);
+        doNothing().when(userValidator).validate(expectedUserToGetSaved);
+
+        Optional<User> expectedFoundSameLoginUserOptional = Optional.empty();
+        when(userDao.findUserByLogin(login)).thenReturn(expectedFoundSameLoginUserOptional);
         //when
-        List<User> actualUserList = userService.getSpecifiedUsers(new NoSpecification<>());
+        userService.signUp(login, password, name, surname, userValidator, passwordValidator);
+        //then
+        verify(userDao, times(1)).saveWithPassword(expectedUserToGetSaved, password);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testSignUpShouldThrowValidationExceptionWhenTheUserIsNotValid() throws DaoException, ValidationException, ServiceException {
+        //given
+        String password = "Password";
+
+        PasswordValidator passwordValidator = mock(PasswordValidator.class);
+        doNothing().when(passwordValidator).validate(password);
+
+        User expectedUserToGetSaved = new User(null, login, name, surname, UserRole.READER, false);
+        UserValidator userValidator = mock(UserValidator.class);
+        doThrow(ValidationException.class).when(userValidator).validate(expectedUserToGetSaved);
+
+        Optional<User> expectedFoundSameLoginUserOptional = Optional.empty();
+        when(userDao.findUserByLogin(login)).thenReturn(expectedFoundSameLoginUserOptional);
+        //when
+        userService.signUp(login, password, name, surname, userValidator, passwordValidator);
+        //then
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testSignUpShouldThrowValidationExceptionWhenThePasswordIsNotValid() throws DaoException, ValidationException, ServiceException {
+        //given
+        String password = "Password";
+
+        PasswordValidator passwordValidator = mock(PasswordValidator.class);
+        doThrow(ValidationException.class).when(passwordValidator).validate(password);
+
+        User expectedUserToGetSaved = new User(null, login, name, surname, UserRole.READER, false);
+        UserValidator userValidator = mock(UserValidator.class);
+        doNothing().when(userValidator).validate(expectedUserToGetSaved);
+
+        Optional<User> expectedFoundSameLoginUserOptional = Optional.empty();
+        when(userDao.findUserByLogin(login)).thenReturn(expectedFoundSameLoginUserOptional);
+        //when
+        userService.signUp(login, password, name, surname, userValidator, passwordValidator);
+        //then
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testSignUpShouldThrowValidationExceptionWhenAUserWithSameLoginAlreadyExists() throws DaoException, ValidationException, ServiceException {
+        //given
+        String password = "Password";
+
+        PasswordValidator passwordValidator = mock(PasswordValidator.class);
+        doNothing().when(passwordValidator).validate(password);
+
+        User expectedUserToGetSaved = new User(null, login, name, surname, UserRole.READER, false);
+        UserValidator userValidator = mock(UserValidator.class);
+        doNothing().when(userValidator).validate(expectedUserToGetSaved);
+
+        Optional<User> expectedFoundSameLoginUserOptional = Optional.of(expectedUserToGetSaved);
+        when(userDao.findUserByLogin(login)).thenReturn(expectedFoundSameLoginUserOptional);
+        //when
+        userService.signUp(login, password, name, surname, userValidator, passwordValidator);
+        //then
+    }
+
+    @Test
+    public void testGetSpecifiedUsersShouldOnlyReturnUsersThatFitTheSpecification() throws DaoException, ServiceException {
+        //given
+        User firstUser = new User(userId, login, name, surname, role, blocked);
+
+        User secondUser = new User(10L, login, name, surname, role, blocked);
+
+        User thirdUser = new User(15L, login, name, surname, role, blocked);
+
+        UserContainsLineSpecification userSpecification = mock(UserContainsLineSpecification.class);
+        when(userSpecification.isSpecified(firstUser)).thenReturn(false);
+        when(userSpecification.isSpecified(secondUser)).thenReturn(true);
+        when(userSpecification.isSpecified(thirdUser)).thenReturn(false);
+
+        List<User> expectedUserList = List.of(secondUser);
+
+        List<User> allUserList = Arrays.asList(firstUser, secondUser, thirdUser);
+        when(userDao.getAll()).thenReturn(allUserList);
+        //when
+        List<User> actualUserList = userService.getSpecifiedUsers(userSpecification);
         //then
         Assert.assertEquals(expectedUserList, actualUserList);
     }
@@ -101,7 +192,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void testSaveUserShouldInvokeSaveMethodOfDaoWhenUserIsValid() throws DaoException, ServiceException, ValidationException {
+    public void testEditUserShouldInvokeSaveMethodOfDaoWhenUserIsValid() throws DaoException, ServiceException, ValidationException {
         //given
         User userToBeSaved = new User(userId, login, name, surname, role, blocked);
 
@@ -114,12 +205,12 @@ public class UserServiceImplTest {
     }
 
     @Test(expected = ValidationException.class)
-    public void testSaveUserShouldThrowValidationExceptionWhenUserIsNotValid() throws ServiceException, ValidationException {
+    public void testEditUserShouldThrowValidationExceptionWhenUserIsNotValid() throws ServiceException, ValidationException {
         //given
         User userToBeSaved = new User(userId, login, name, surname, role, blocked);
 
         UserValidator userValidator = mock(UserValidator.class);
-        doThrow(new ValidationException()).when(userValidator).validate(userToBeSaved);
+        doThrow(ValidationException.class).when(userValidator).validate(userToBeSaved);
         //when
         userService.editUser(userId, login, name, surname, role, blocked, userValidator);
         //then
