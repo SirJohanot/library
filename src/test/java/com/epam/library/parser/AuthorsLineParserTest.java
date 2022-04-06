da9f4437ee6152661e6c57043c60b1d1208045f8
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-04-06 19:10:30 +0300

    added tests to AuthorsLineParserTest

--- src/test/java/com/epam/library/parser/AuthorsLineParserTest.java
+++ src/test/java/com/epam/library/parser/AuthorsLineParserTest.java
@@ -9,16 +9,16 @@ import java.util.List;
 
 public class AuthorsLineParserTest {
 
-    private final Author firstAuthor = Author.ofName("Leo Tolstoy");
-    private final Author secondAuthor = Author.ofName("Pavel Tsivunchyk");
-    private final Author thirdAuthor = Author.ofName("George Orwell");
-    private final List<Author> authorList = Arrays.asList(firstAuthor, secondAuthor, thirdAuthor);
     private final AuthorsLineParser authorsLineParser = new AuthorsLineParser();
 
     @Test
     public void testParseShouldIgnoreAllSpacesThatNeighbourTheCommandWhenTheAuthorsLineContainsMultipleCommas() {
         //given
         String authorsLine = "Leo Tolstoy     ,         Pavel Tsivunchyk      ,         George Orwell";
+        Author firstAuthor = Author.ofName("Leo Tolstoy");
+        Author secondAuthor = Author.ofName("Pavel Tsivunchyk");
+        Author thirdAuthor = Author.ofName("George Orwell");
+        List<Author> authorList = Arrays.asList(firstAuthor, secondAuthor, thirdAuthor);
         //when
         List<Author> actualAuthorList = authorsLineParser.parse(authorsLine);
         //then
@@ -29,6 +29,39 @@ public class AuthorsLineParserTest {
     public void testParseShouldParseTheAuthorsLineWhenItDoesNotHaveSpacesBeforeOfAfterAuthors() {
         //given
         String authorsLine = "Leo Tolstoy,Pavel Tsivunchyk,George Orwell";
+        Author firstAuthor = Author.ofName("Leo Tolstoy");
+        Author secondAuthor = Author.ofName("Pavel Tsivunchyk");
+        Author thirdAuthor = Author.ofName("George Orwell");
+        List<Author> authorList = Arrays.asList(firstAuthor, secondAuthor, thirdAuthor);
+        //when
+        List<Author> actualAuthorList = authorsLineParser.parse(authorsLine);
+        //then
+        Assert.assertEquals(authorList, actualAuthorList);
+    }
+
+    @Test
+    public void testParseShouldReturnListWithAnEmptyStringIfThereIsNoAuthorBetweenTwoCommas() {
+        //given
+        String authorsLine = "Leo Tolstoy,              ,George Orwell";
+        Author firstAuthor = Author.ofName("Leo Tolstoy");
+        Author secondAuthor = Author.ofName("");
+        Author thirdAuthor = Author.ofName("George Orwell");
+        List<Author> authorList = Arrays.asList(firstAuthor, secondAuthor, thirdAuthor);
+        //when
+        List<Author> actualAuthorList = authorsLineParser.parse(authorsLine);
+        //then
+        Assert.assertEquals(authorList, actualAuthorList);
+    }
+
+    @Test
+    public void testParseShouldReturnListWithEmptyStringsIfThereAreEmptyAuthors() {
+        //given
+        String authorsLine = "Leo Tolstoy,              ,,George Orwell";
+        Author firstAuthor = Author.ofName("Leo Tolstoy");
+        Author secondAuthor = Author.ofName("");
+        Author thirdAuthor = Author.ofName("");
+        Author fourthAuthor = Author.ofName("George Orwell");
+        List<Author> authorList = Arrays.asList(firstAuthor, secondAuthor, thirdAuthor, fourthAuthor);
         //when
         List<Author> actualAuthorList = authorsLineParser.parse(authorsLine);
         //then
