5a429f1b0582dc27e0e9537f2a9162f510017145
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-09-29 19:01:35 +0300

    removed accidentally versioned directories

--- target/library/static/javaScript/daysToggling.js
+++
@@ -1,6 +0,0 @@
-function setDaysRadiosDisabled(disabled) {
-    let daysRadios = document.getElementsByName("days");
-    for (let i = 0; i < daysRadios.length; i++) {
-        daysRadios[i].disabled = Boolean(disabled);
-    }
-}
